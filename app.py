b53c83f7b8f348c28b4dccb7ad7876dbb23a6a75
root <santiagoagustinfernandez@gmail.com>
2020-06-08 17:34:15 -0300

    Sin Password

---
+++ app.py
@@ -0,0 +1,97 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Configuracion con SQLite Sin nada.
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'crud.sqlite')
+# Configuracion con MySQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://USER:PASS@localhost/test_api'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('username', 'email')
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+# Enpoint Creacion de Usuario
+@app.route("/user", methods=["POST"])
+def add_user():
+    username = request.json['username']
+    email = request.json['email']
+
+    new_user = User(username, email)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    User_dict = {
+        "username": new_user.username,
+        "email": new_user.email
+    }
+
+    return jsonify(User_dict)
+
+
+# Enpoint Muestra todos los Usuarios
+@app.route("/user", methods=["GET"])
+def get_user():
+    all_users = User.query.all()
+    result = users_schema.dump(all_users)
+    return jsonify(result.data)
+
+
+# Enpoint Muestra Detalle de los Usuarios por ID
+@app.route("/user/<id>", methods=["GET"])
+def user_detail(id):
+    user = User.query.get(id)
+    return user_schema.jsonify(user)
+
+
+# Enpoint Modifica Usuario por ID
+@app.route("/user/<id>", methods=["PUT"])
+def user_update(id):
+    user = User.query.get(id)
+    username = request.json['username']
+    email = request.json['email']
+
+    user.email = email
+    user.username = username
+
+    db.session.commit()
+    return user_schema.jsonify(user)
+
+
+# Enpoint Elimina Usuario
+@app.route("/user/<id>", methods=["DELETE"])
+def user_delete(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return user_schema.jsonify(user)
+
+
+if __name__ == '__main__':
+    db.create_all() # Una vez que crea las tablas, en base a SQLAlchemy, podemos eliminarlo.
+    app.run(host='0.0.0.0') # Ponemos 0.0.0.0 para que tenga acceso, de cualquier lado.
